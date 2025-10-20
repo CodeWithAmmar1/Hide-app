7ac5cd258dd0f63f58ca0c69a64cddb38c3b3ab5
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-10-20 11:35:31 +0500

    update

---
+++ lib/view/music/widget/songtiltle.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SongTile extends StatelessWidget {
+  final String title;
+  final String artist;
+  final VoidCallback onTap;
+
+  const SongTile({
+    super.key,
+    required this.title,
+    required this.artist,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.music_note),
+      title: Text(title, maxLines: 1, overflow: TextOverflow.ellipsis),
+      subtitle: Text(artist),
+      onTap: onTap,
+    );
+  }
+}
