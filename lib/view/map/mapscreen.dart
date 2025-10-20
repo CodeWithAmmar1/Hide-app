fd850110f60b2f917d2b5aee1149a27af5cfd9a7
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-10-20 12:00:40 +0500

    map appbar change

--- lib/view/map/mapscreen.dart
+++ lib/view/map/mapscreen.dart
@@ -6,16 +6,19 @@ class MapScreen extends StatelessWidget {
   final double latitude;
   final double longitude;
 
-  const MapScreen({
-    super.key,
-    required this.latitude,
-    required this.longitude,
-  });
+  const MapScreen({super.key, required this.latitude, required this.longitude});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Map (MapTiler)")),
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        centerTitle: true,
+        title: const Text(
+          "Map View",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
       body: FlutterMap(
         options: MapOptions(
           initialCenter: LatLng(latitude, longitude),
