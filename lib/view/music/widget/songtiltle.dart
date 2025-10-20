0f0a219d985655a53988064069b41fa4c6c58523
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-10-20 12:01:19 +0500

    change theme

--- lib/view/music/widget/songtiltle.dart
+++ lib/view/music/widget/songtiltle.dart
@@ -15,7 +15,7 @@ class SongTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: const Icon(Icons.music_note),
+      leading: const Icon(Icons.music_note,color: Colors.orange,),
       title: Text(title, maxLines: 1, overflow: TextOverflow.ellipsis),
       subtitle: Text(artist),
       onTap: onTap,
